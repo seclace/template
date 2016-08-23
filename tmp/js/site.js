30ce668ba6ec758fecb076dd9b23c163370a90d5
seclace <seclace@mail.ru>
2016-08-23 23:15:09 +0700

    Clear template for new projects

--- tmp/js/site.js
+++ tmp/js/site.js
@@ -1,15 +1,3 @@
 $(document).ready(function () {
-    $('.products-block .products-slider').owlCarousel({
-        items: 3,
-        nav: true,
-        navText: ['',''],
-        dots: false,
-        margin: 14
-    });
-    $('.main-specials-slider').owlCarousel({
-        items: 1,
-        nav: true,
-        navText: ['',''],
-        dots: true
-    });
+    
 });
